d8904e4b890c0106c82c80d7d474d1bbd8c72ca7
David <davidghazaryan2000@gmail.com>
2020-09-03 21:24:54 +0400

    mongoDB connection and mongo Models setup

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const cors = require('cors')
 
+const mongoose = require('mongoose')
+
 const app = express()
 app.use(cors())
 
 app.use(express.json({extended: true}))
+
 const PORT = process.env.NODE_PORT || 5000
 
 async function run() {
     try {
-
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
         app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
     } catch (e) {
         console.log('Dear Developer catched a SERVER ERROR...', e.message, e);
