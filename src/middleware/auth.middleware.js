56972dd5661043dbc08eb98218faeec6a8406b31
David <davidghazaryan2000@gmail.com>
2020-09-03 21:26:49 +0400

    auth middleware setup

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+module.exports = (req, res, next) => {
+    if(req.method === "OPTIONS"){
+        return next();
+    }
+
+    try {
+        const token = req.headers.authorization.split(' ')[1]; // "Bearer TOKEN"
+        if(!token) {
+            return res.status(401).json({message: 'Not authorized'});
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+
+        next();
+    }catch (e) {
+        console.log(e);
+        return res.status(401).json({message: 'Not authorized'});
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
